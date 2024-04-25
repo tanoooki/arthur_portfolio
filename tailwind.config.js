7078fcede1f2d87db38b5d771e179a8046f2fa04
Tanoooki <schritke@hotmail.com>
2024-04-25 11:53:46 -0300

    correção tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+  content: [
+    './**/*.{html,js,css}',
+  ],
     theme: {
       fontFamily: {
         'sans': ['Kanit', 'sans-serif'],
